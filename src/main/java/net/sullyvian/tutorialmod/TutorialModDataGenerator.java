901155b56ba7a402c3d52569340b959522f85690
Vianney HERVY on COMPUTAH <vianney.hervy@etu.inp-n7.fr>
2024-11-03 20:19:34 +0100

    moved all data files into backup, We're gonna generate them waaaaaw

--- src/main/java/net/sullyvian/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/sullyvian/tutorialmod/TutorialModDataGenerator.java
@@ -3,6 +3,9 @@ package net.sullyvian.tutorialmod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+/**
+ * Generates all blockstates, models and data json files
+ */
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
